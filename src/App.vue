28b39714994cf10e4139ace9fd16c19242fd24ef
张营 <zy_carl@formssi.com>
2022-05-12 11:47:35 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
+import SwaggerParser from "@apidevtools/swagger-parser";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld
+  },
+  mounted() {
+    axios
+      .get("/api/demo/v2/api-docs", {
+        headers: {
+          Authorization: "Bearer 9DB9F7BE-72F7-4DEF-904A-072F3C4720B3",
+        },
+      })
+      .then((res) => {
+        let swaggerParser = new SwaggerParser();
+        let options = {
+          parse: {
+            json: true,
+            yaml: true,
+            text: true,
+            binary: true,
+          },
+          resolve: {
+            external: true,
+          },
+          dereference: {
+            circular: true,
+          },
+          validate: {
+            schema: false,
+            spec: false,
+          },
+        };
+        swaggerParser.validate(res.data, options).then((res) => {
+          console.log(res);
+        });
+      });
+  },
+};
 </script>
 
 <style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: 'http://172.21.93.222'
+  },
+  configureWebpack: {
+    plugins: [
+      new NodePolyfillPlugin()
+    ]
+  }
 })
